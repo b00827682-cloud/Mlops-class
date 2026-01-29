209204d34800287bf50817e3a4770417a1ad532a
marcakra2002-beep <marcakra2002@gmail.com>
2026-01-29 11:51:03 +0100

    Initial commit

---
+++ MLOPS.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Surface API")
+
+@app.get("/surface{surface}")
+def surface(surface: int):
+    api = 1 if surface <= 50 else 2
+    return {"surface": surface, "api": api}
+
